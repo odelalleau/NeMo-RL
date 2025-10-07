aaa25ed47c8d6b2ad1c2e44745f0f775ca441687
Yi-Fu Wu <yifu.wu@gmail.com>
2025-10-07 08:40:16 -0700

    Set context_parallel_size when importing model
    Prevents incorrect dp size in parallel_state during initial import.
    
    Signed-off-by: Yi-Fu Wu <yifu.wu@gmail.com>

--- nemo_rl/models/megatron/community_import.py
+++ nemo_rl/models/megatron/community_import.py
@@ -39,6 +39,7 @@ def import_model_from_hf_name(
     # Keep track of defaults so can restore them to the config after loading the model
     orig_tensor_model_parallel_size = model_provider.tensor_model_parallel_size
     orig_pipeline_model_parallel_size = model_provider.pipeline_model_parallel_size
+    orig_context_parallel_size = model_provider.context_parallel_size
     orig_expert_model_parallel_size = model_provider.expert_model_parallel_size
     orig_expert_tensor_parallel_size = model_provider.expert_tensor_parallel_size
     orig_num_layers_in_first_pipeline_stage = (
@@ -55,6 +56,9 @@ def import_model_from_hf_name(
         ]
         model_provider.pipeline_model_parallel_size = megatron_config[
             "pipeline_model_parallel_size"
+        ]
+        model_provider.context_parallel_size = megatron_config[
+            "context_parallel_size"
         ]
         model_provider.expert_model_parallel_size = megatron_config[
             "expert_model_parallel_size"
@@ -78,6 +82,7 @@ def import_model_from_hf_name(
     config = megatron_model[0].config
     config.tensor_model_parallel_size = orig_tensor_model_parallel_size
     config.pipeline_model_parallel_size = orig_pipeline_model_parallel_size
+    config.context_parallel_size = orig_context_parallel_size
     config.expert_model_parallel_size = orig_expert_model_parallel_size
     config.expert_tensor_parallel_size = orig_expert_tensor_parallel_size
     config.num_layers_in_first_pipeline_stage = orig_num_layers_in_first_pipeline_stage
